14a07168e57ea95e76bc0c00d976c81b5c269b2d
Galina Bondar <galina.rizhniak@gmai.com>
2021-06-01 22:13:56 +0300

    Add main files

---
+++ myjavascript.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function DataTable(config, data) {
+    // содержимое этой функции вам и нужно написать :)
+}
+
+const config1 = {
+    parent: '#usersTable',
+    columns: [
+        { title: 'Имя', value: 'name' },
+        { title: 'Фамилия', value: 'surname' },
+        { title: 'Возраст', value: 'age' },
+    ]
+};
+
+const users = [
+    { id: 30050, name: 'Вася', surname: 'Петров', age: 12 },
+    { id: 30051, name: 'Вася', surname: 'Васечкин', age: 15 },
+];
+
+DataTable(config1, users);
